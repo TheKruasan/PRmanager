65d2643de81dfd974b241574b3f4fe7656ca960b
artem <artemrusskin92@gmail.com>
2025-11-25 19:44:37 +0300

    add readme

--- internal/models/team.go
+++ internal/models/team.go
@@ -2,5 +2,5 @@ package models
 
 type Team struct {
 	TeamName string `json:"team_name"`
-	Members  []User `json:"members"` // Теперь просто []User вместо []TeamMember
+	Members  []User `json:"members"`
 }
